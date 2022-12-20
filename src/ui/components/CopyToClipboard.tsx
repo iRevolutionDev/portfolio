c7ce0a75de3f7541bebaa842104c8bb98cfa7db4
iRevolutionDev <tavinho.daniel@gmail.com>
2022-12-19 22:43:56 -0600

    feat: added copy to clipboard component and hack the box social media.

---
+++ src/ui/components/CopyToClipboard.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from "react";
+
+type Props = {
+    text: string;
+    children?: React.ReactNode;
+}
+
+const CopyToClipboard: React.FC<Props> = ({ text, children }) => {
+
+    const _copyToClipboard = async (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();
+        await navigator.clipboard.writeText(text)
+    }
+
+    return (
+        <span onClick={_copyToClipboard} className="cursor-pointer">
+            {children}
+        </span>
+    )
+}
+
+export default CopyToClipboard;
