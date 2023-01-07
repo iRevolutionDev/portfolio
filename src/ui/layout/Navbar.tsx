b102e637c6d8616916a700eac81f1631b09224fe
iRevolutionDev <tavinho.daniel@gmail.com>
2023-01-06 21:44:43 -0600

    feat: added menu in mobile and changed hamburger menu icon

--- src/ui/layout/Navbar.tsx
+++ src/ui/layout/Navbar.tsx
@@ -1,22 +1,71 @@
 'use client';
 
-import React, {useState} from "react";
-import {Cross as Hamburger} from 'hamburger-react';
+import React, {useEffect, useState} from "react";
+import {Squash as Hamburger} from 'hamburger-react';
+import {SiSpotify} from "react-icons/all";
 import Link from "next/link";
 
 const Mobile: React.FC = () => {
     const [isOpen, setOpen] = useState(false);
+    const [onTop, setOnTop] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setOnTop(window.scrollY > 0);
+        }
+
+        window.addEventListener('scroll', handleScroll, {passive: true});
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <div className="sticky top-0 z-20 h-32 overflow-hidden transition-all sm:hidden">
-            <div className="mx-5 mt-10 relative transition-all bg-ctp-surface0 rounded-lg bg-dark-50 backdrop-blur-xl opacity-40">
-                <div className="flex justify-between space-x-2 pr-5 transition-colors">
-                    <button className="relative z-50 block px-2 text-neutral-500 transition-all focus:ring">
-                        <Hamburger onToggle={toggled => setOpen(toggled)} />
-                    </button>
+        <>
+            {isOpen ? (<div className="fixed inset-0 z-10 space-y-2 py-24 px-8 sm:hidden bg-dark-100">
+                <h1 className="text-2xl font-bold text-white">Pages</h1>
+                <ul className="grid grid-cols-1 gap-2">
+                    <li className="shrink-0">
+                        <Link href="/" className="block py-3 font-mono text-lg hover:text-ctp-text no-underline sm:hover:bg-ctp-text/20 rounded-md sm:inline-block
+                                           sm:px-5 sm:text-sm sm:font-normal sm:hover:bg-ctp-text/20 sm:rounded-full">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="shrink-0">
+                        <Link href="/about" className="block py-3 font-mono text-lg hover:text-ctp-text no-underline sm:hover:bg-ctp-text/20 rounded-md sm:inline-block
+                                             sm:px-5 sm:text-sm sm:font-normal sm:hover:bg-ctp-text/20 sm:rounded-full">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/blog" className="block py-3 font-mono text-lg hover:text-ctp-text no-underline sm:hover:bg-ctp-text/20 rounded-md sm:inline-block
+                                                  sm:px-5 sm:text-sm sm:font-normal sm:hover:bg-ctp-text/20 sm:rounded-full">
+                            Blog
+                        </Link>
+                    </li>
+                </ul>
+            </div>) : null}
+
+            <div className="sticky top-0 z-20 h-32 overflow-hidden transition-all sm:hidden">
+                <div className={onTop || isOpen ? "mt-0 relative transition-all bg-ctp-surface0 rounded-none" : "mx-5 mt-10 relative transition-all bg-ctp-surface0 rounded-lg"}>
+                    <div className="flex justify-between space-x-2 pr-5 transition-colors bg-transparent">
+                        <button className="relative z-50 block px-2 text-neutral-500 transition-all focus:ring">
+                            <Hamburger size={24} onToggle={toggled => setOpen(toggled)} />
+                        </button>
+                        <div className="overflow-hidden py-2 px-1">
+                            <p className="text-right select-none sm:select-text w-full inline-flex text-lg rounded-sm focus:outline-none focus:opacity-100
+                                      focus:ring items-center space-x-2 no-underline opacity-50 hover:opacity-100 h-12 mt-0.5">
+                                <span>Not playing anything</span>
+                                <span>
+                                <SiSpotify/>
+                            </span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
