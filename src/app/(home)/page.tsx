5d5d38cd588d1276988fd86d3719ff894fb6c98a
Revolution <tavinho.daniel@gmail.com>
2023-07-27 23:38:17 -0300

    fix: adjust responsive design for links and localization

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,9 +7,9 @@ import JobDetailsCard from "@/components/job-details-card";
 
 export default function Page() {
     return (
-        <main className="mx-auto max-w-3xl space-y-12 md:py-24">
+        <main className="mx-auto max-w-3xl space-y-12 py-24">
             <Stack direction="column" spacing={2}>
-                <Stack direction="row" spacing={2} alignContent="center" alignItems="center">
+                <Stack direction={{xs: 'column', sm: 'row'}} spacing={2} alignContent="center" alignItems="center">
                     <SocialLinks/>
                     <LocationCard/>
                 </Stack>
