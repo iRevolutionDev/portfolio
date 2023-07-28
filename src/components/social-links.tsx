fa70ee4687caa3e89e4e5d349fcb4e749f7da2a7
Revolution <tavinho.daniel@gmail.com>
2023-07-28 13:23:19 -0300

    fix: Spotify watcher and social responsive content

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -4,19 +4,21 @@ import {FC} from "react";
 import {GitHub, LinkedIn} from "@mui/icons-material";
 import {SiSpotify} from "react-icons/si";
 import Link from "next/link";
+import {Stack} from "@mui/material";
 
-export const SocialLinks: FC = ({  }) => {
+export const SocialLinks: FC = ({}) => {
     return (
-        <>
+        <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
             <Link href="https://github.com/irevolutiondev" target="_blank" rel="noopener noreferrer">
                 <GitHub fontSize="large"/>
             </Link>
             <Link href="https://www.linkedin.com/in/revolutionxk/" target="_blank" rel="noopener noreferrer">
                 <LinkedIn fontSize="large"/>
             </Link>
-            <Link href="https://open.spotify.com/user/jcm4iped0mnqusg2cvq5hy26z" target="_blank" rel="noopener noreferrer">
+            <Link href="https://open.spotify.com/user/jcm4iped0mnqusg2cvq5hy26z" target="_blank"
+                  rel="noopener noreferrer">
                 <SiSpotify fontSize="1.75rem"/>
             </Link>
-        </>
+        </Stack>
     );
 }
