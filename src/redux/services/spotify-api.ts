a623533e75d25c59ff439d26dbaad33937cbe46c
Revolution <tavinho.daniel@gmail.com>
2023-07-28 13:42:43 -0300

    fix: base url is undefined

--- src/redux/services/spotify-api.ts
+++ src/redux/services/spotify-api.ts
@@ -1,13 +1,12 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {SpotifyResponse} from "@/@types/spotify-response";
-import {WEBSITE_URL} from "@/helpers/constants";
 
 export const spotifyApi = createApi({
     reducerPath: "spotifyApi",
     refetchOnFocus: true,
     refetchOnReconnect: true,
     baseQuery: fetchBaseQuery({
-        baseUrl: `${WEBSITE_URL}/api/`,
+        baseUrl: `/api/`,
     }),
     endpoints: (builder) => ({
         getTracks: builder.query<SpotifyResponse, void>({
