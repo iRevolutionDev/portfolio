75e9ce8a0632c6e1a3d58700068d67730ce68178
Revolution <tavinho.daniel@gmail.com>
2023-08-01 20:52:57 -0300

    feat: add autoFocus to command input in terminal

--- src/components/terminal/command-input.tsx
+++ src/components/terminal/command-input.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 
 type CommandInputProps = {
     onExecute: (command: string) => void
@@ -6,6 +6,19 @@ type CommandInputProps = {
 
 export const CommandInput: FC<CommandInputProps> = ({onExecute}) => {
     const [command, setCommand] = useState<string>("")
+    const inputRef = React.createRef<HTMLInputElement>()
+
+    useEffect(() => {
+        window.addEventListener('click', () => {
+            inputRef.current?.focus()
+        })
+
+        return () => {
+            window.removeEventListener('click', () => {
+                inputRef.current?.focus()
+            })
+        }
+    }, [inputRef])
 
     const sendCommand = () => {
         if (command.length <= 0) return
@@ -23,7 +36,7 @@ export const CommandInput: FC<CommandInputProps> = ({onExecute}) => {
     return (
         <input type="text" className="bg-transparent outline-none border-none w-full h-full ml-2"
                value={command} onChange={e => setCommand(e.target.value)}
-               onKeyDown={onKeyDown}
+               onKeyDown={onKeyDown} ref={inputRef} autoFocus={true}
         />
     )
 }
