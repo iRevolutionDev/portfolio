56bef80092011694d96e9fa19ded6f752d4b74d5
Revolution <tavinho.daniel@gmail.com>
2024-08-19 01:07:46 -0300

    feat: add better mobile app bar transitions and fix flickering

--- src/components/mobile-menu.tsx
+++ src/components/mobile-menu.tsx
@@ -4,50 +4,40 @@ import { LanguageSwitch } from "@/components/language-switch";
 import { ToggleThemeButton } from "@/components/toggle-theme-button";
 import { useAppSelector } from "@/redux/hooks";
 import { Stack } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import type { FC, PropsWithChildren } from "react";
 
 const MobileMenu: FC<PropsWithChildren> = ({ children }) => {
 	const { open } = useAppSelector((state) => state.menu);
-	const theme = useTheme();
 
 	return (
-		<>
+		<AnimatePresence mode="wait">
 			{open && (
 				<motion.div
-					className="fixed inset-0 bg-black z-50"
+					className="fixed inset-0 bg-black z-50 backdrop-blur-md"
 					style={{
-						backgroundColor: theme.palette.background.default,
-						opacity: 0.5,
+						backgroundColor:
+							"rgba(from var(--mui-palette-background-paper) r g b / 90%)",
 					}}
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-					transition={{ duration: 0.2 }}
+					initial={{ y: "-100%" }}
+					animate={{ y: 0 }}
+					exit={{ y: "-100%" }}
+					transition={{ duration: 0.3 }}
 				>
-					<motion.div
-						className="fixed inset-0 z-50"
-						initial={{ y: "-100%" }}
-						animate={{ y: 0 }}
-						exit={{ y: "100%" }}
-						transition={{ duration: 0.2 }}
-					>
-						<div className="flex flex-col w-full h-full my-20 relative">
-							<ul className="list-none w-full">{children}</ul>
-							<div className="relative w-full h-full">
-								<div className="absolute right-4 bottom-[6rem]">
-									<Stack direction="column" spacing={2}>
-										<LanguageSwitch />
-										<ToggleThemeButton />
-									</Stack>
-								</div>
+					<div className="flex flex-col w-full h-full my-20 relative">
+						<ul className="list-none w-full">{children}</ul>
+						<div className="relative w-full h-full">
+							<div className="absolute right-4 bottom-[6rem]">
+								<Stack direction="column" spacing={2}>
+									<LanguageSwitch />
+									<ToggleThemeButton />
+								</Stack>
 							</div>
 						</div>
-					</motion.div>
+					</div>
 				</motion.div>
 			)}
-		</>
+		</AnimatePresence>
 	);
 };
 
