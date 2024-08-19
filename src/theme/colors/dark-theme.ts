a8c9a5f8536a59b2979547af175209045a394078
Revolution <tavinho.daniel@gmail.com>
2024-08-18 21:03:58 -0300

    feat: add deepmerge for merge common theme colors

--- src/theme/colors/dark-theme.ts
+++ src/theme/colors/dark-theme.ts
@@ -1,8 +1,9 @@
 import { commonTheme } from "@/theme/colors/common-theme";
+import type { ThemeOptions } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
+import { deepmerge } from "@mui/utils";
 
-export const darkTheme = createTheme({
-	...commonTheme,
+const darkThemeOptions: ThemeOptions = {
 	palette: {
 		mode: "dark",
 		primary: {
@@ -65,4 +66,6 @@ export const darkTheme = createTheme({
 		"0px 11px 14px -7px rgba(0,0,0,0.2),0px 23px 36px 3px rgba(0,0,0,0.14),0px 9px 44px 8px rgba(0,0,0,0.12)",
 		"0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)",
 	],
-});
+};
+
+export const darkTheme = createTheme(deepmerge(commonTheme, darkThemeOptions));
