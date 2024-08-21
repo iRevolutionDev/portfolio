d03ac32f7b11235756f4d6f1bb6e743a4109a7c6
Revolution <tavinho.daniel@gmail.com>
2024-08-21 00:20:25 -0300

    refactor: change package from `app` to `next_api`

--- api/spotify/me.rs
+++ api/spotify/me.rs
@@ -1,4 +1,4 @@
-use app::services::spotify_service::{SpotifyService, SPOTIFY_SERVICE};
+use next_api::services::spotify_service::{SpotifyService, SPOTIFY_SERVICE};
 use tokio::sync::Mutex;
 use vercel_runtime::{process_request, process_response, run_service, service_fn, Body, Error, Request, Response, ServiceBuilder};
 
